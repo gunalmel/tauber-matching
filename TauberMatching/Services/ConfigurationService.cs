17d0a173ccc65f3dce799043fba8013559375e65
Melih Gunal <gunalmel@yahoo.com>
2011-11-07 01:41:30 -0500

    ConfigurationService Get and Update methods are finished. Not covered by tests.

--- TauberMatching/Services/ConfigurationService.cs
+++ TauberMatching/Services/ConfigurationService.cs
@@ -11,20 +11,31 @@ namespace TauberMatching.Services
     /// </summary>
     public class ConfigurationService
     {
-        MatchingDB db = new MatchingDB();
         /// <summary>
         /// Retrieves all application settings in AppConfiguration object
         /// </summary>
         /// <returns>AppConfiguration object that encapsulates all named application configuration parameters.</returns>
         public AppConfiguration GetConfigParameters()
         {
-            var appConfig = new AppConfiguration(db.ConfigParameters);
+            AppConfiguration appConfig;
+            using (MatchingDB db = new MatchingDB())
+                appConfig = new AppConfiguration(db.ConfigParameters);
             return appConfig;
         }
-
+        /// <summary>
+        /// Updates the config parameters in the database matching the properties of the method argument
+        /// </summary>
+        /// <param name="config">AppCOnfiguration object encapsulating application configuration parameters to be set in the db.</param>
         public void UpdateConfigParameters(AppConfiguration config)
         {
-            
+            using(MatchingDB db = new MatchingDB())
+            {
+                foreach (ConfigParameter p in config.GetConfigParameters())
+                {
+                    db.ConfigParameters.Where(pr=>pr.Id==p.Id).First().Value=p.Value;
+                    db.SaveChanges();
+                }
+            }
         }
     }
 }
